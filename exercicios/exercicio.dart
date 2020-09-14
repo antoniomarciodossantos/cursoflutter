9d41fe24b3d01c4e30392d6fc8c715dafb6cf83b
marciosantos_ammo <marcio.santos@ammovarejo.com.br>
2020-09-14 08:55:38 -0300

    primeiro commit - subida das pastas

---
+++ exercicios/exercicio.dart
@@ -0,0 +1,7 @@
+
+main(){
+  final r = soma(2,5);
+  print('a soma é : ${soma(2,5)}'); 
+  print('a soma é : $r'); 
+  
+}
---
+++ projeto1/projeto_perguntas_respostas/lib/main.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
+
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  
+  var _perguntaSelecionada = 0;
+
+  void _responder(){
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
+  //teste
+
+  @override
+  Widget build(BuildContext context){
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita',
+      'Qual é seu animal favorito',
+      'outro item',
+      
+      
+    ];
+
+    return MaterialApp(
+      home: Scaffold(    
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Questao(perguntas[_perguntaSelecionada]),
+            Resposta('Resposta 11'),
+            Resposta('Resposta 22'),
+            Resposta('Resposta 33')
+            //RaisedButton(
+            //  child: Text('Resposta 1'),
+            //  onPressed: _responder,
+            //),
+            //RaisedButton(
+            //  child: Text('Resposta 2'),
+            //  onPressed: _responder,
+            //),
+            //RaisedButton(
+            //  child: Text('Resposta 3'),
+            //  onPressed: _responder,
+            //)
+
+          ],
+        ),
+      )
+    );
+  }
+
+}
+
+
+class PerguntaApp extends StatefulWidget {
+
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+
+}
